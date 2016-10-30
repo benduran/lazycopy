37b8d1200f298477ec9aa7aa0d440a95c5b17dfa
Benjamin Duran <Benjamin Duran>
2016-10-30 14:11:10 -0700

    Initial commit...and I think it's pretty much done :) Will need to write some unit tests to verify.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# lazycopy
+## lazycopy
 This is a simple CLI utility for copying all files from one directory to another. Allows for overwriting files in src directory if they already exist.
+
+### Motivation
+I'm super lazy and I have a huge music library. Rather than manually copying files to my NAS when I want to backup my latest music collection changes, I wrote a simple utility to automate this for me so I can leave it running in the background. This utility will also maintain the relative file tree structure when copying files.
+
+## CLI Arguments
+- `--from` - `{string}` - Folder path with files you want to copy
+- `--to` - `{string}` - Folder destination where you want all of the files to go. Folder will be created if it doesn't already exist. All nested file tree paths will be created inside of this folder to mirror the `--from` path.
+- `--overwrite` - `{optional}` - Override files in the `--to` path if they already exist. Leave them alone if otherwise.
---
+++ index.js
@@ -0,0 +1,95 @@
+'use strict';
+
+import path from 'path';
+
+import minimist from 'minimist';
+import glob from 'glob';
+import fs from 'fs-extra';
+
+const fromCli = !module.parent; // If there isn't a parent module, then it was run directly from the CLI
+
+function copydiff(args = {}) {
+    return new Promise((resolve, reject) => {
+        if (fromCli) {
+            args = minimist(process.argv.slice(2));
+        }
+        const {
+            o,
+            overwrite,
+            from,
+            to
+        } = args;
+        const overwriteFiles = o || overwrite || false;
+        if (!from) {
+            reject(new Error('No "from" provided to copydiff'));
+        }
+        if (!to) {
+            reject(new Error('No "to" provided to copydiff'));
+        }
+        const fromResolved = path.resolve(from);
+        const toResolved = path.resolve(to);
+        const useForwardSlash = fromResolved.lastIndexOf('/') > -1;
+        let scanPath = fromResolved;
+        if (fs.statSync(fromResolved).isDirectory()) {
+            scanPath += '/**/*.*';
+        }
+        glob(path.resolve(scanPath), (error, results) => {
+            if (error) {
+                reject(error);
+            }
+            else {
+                let bar = null,
+                    allPromises = [];
+                if (fromCli) {
+                    console.log(`${results.length} files to be copied`);
+                }
+                // Check if the file is already existing in the new location
+                let counter = 0;
+                results.forEach((r) => {
+                    const def = new Promise((resolve, reject) => {
+                        const resolvedPath = path.resolve(r);
+                        const relPath = resolvedPath.replace(fromResolved, '');
+                        // We now have the relative path to the FROM area, so we should be able to copy this over to the TO area
+                        const computedToPath = `${toResolved}${relPath}`;
+                        try {
+                            fs.copySync(resolvedPath, computedToPath, {
+                                clobber: overwriteFiles,
+                                preserveTimestamps: true
+                            });
+                            counter++;
+                            process.stdout.write(`${counter} of ${results.length} files copied\r`); // This makes us continue outputting on the same line in the CLI
+                            resolve();
+                        }
+                        catch (error) {
+                            reject(error);
+                        }
+                    });
+                    allPromises.push(def);
+                });
+                Promise.all(allPromises).then(() => {
+                    if (fromCli) {
+                        console.log(`All files have been copied to ${toResolved}`);
+                    }
+                    resolve();
+                }).catch((error) => {
+                    if (fromCli) {
+                        console.log(error.stack);
+                        console.error(error);
+                    }
+                    reject(error);
+                });
+            }
+        });
+    });
+}
+
+export default copydiff;
+
+if (fromCli) {
+    Promise.resolve(copydiff()).then(() => {}).catch((error) => {
+        console.log(error.stack);
+        console.error(error);
+    }).then(() => {
+        process.exit();
+    });
+}
