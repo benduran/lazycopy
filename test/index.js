acef8b94a59d32ffb4ac6b8dceef1d7c4dd2f0df
Benjamin Duran <Benjamin Duran>
2016-12-21 16:41:25 -0800

    - Added a new test to ignore all javacript files

--- test/index.js
+++ test/index.js
@@ -52,6 +52,30 @@ function runAll() {
                 }, 10000);
             });
         }).timeout(30000);
+        it('Should asynchronously copy everything from the node_modules folder, excluding all js files', () => {
+            const src = path.resolve(path.join(__dirname, '../node_modules/**/*.!(js)'));
+            const testAgainst = glob.sync(src);
+            return copier().copy([{
+                src,
+                dest: path.join(__dirname, '../testing'),
+                cwd: path.join(__dirname, '../')
+            }]).then(() => {
+                // May need to wait for all files to be copied, as the code will likely finish executing before the OS
+                // kernel has actually performed the file I/O
+                // This may not pass on non SSD machines because the I/O time will be substantially longer
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        const testPath = path.resolve(path.join(__dirname, '../testing/node_modules/**/*.!(js)'));
+                        const copiedFiles = glob.sync(testPath);
+                        console.info(`${testAgainst.length} equal to ${copiedFiles.length}`);
+                        expect(copiedFiles.length).to.be.equal(testAgainst.length);
+                        resolve();
+                    }, 10000);
+                });
+            }).catch((error) => {
+                expect(error).to.not.be.an('error');
+            });
+        }).timeout(30000);
         afterEach(() => {
             fs.removeSync(path.join(__dirname, '../testing'));
         });
